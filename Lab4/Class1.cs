ef0cf1ba90aee1d3434ea44462a4047269ed58f0
MiroshnichenkoNA <rainbow00863@mail.ru>
2022-05-09 19:46:36 +0700

    added GameLibrary, Object creation test successes

---
+++ Lab4/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameLibrary;
+namespace GameLibrary
+{
+    public class Class1
+    {
+        public char[,] field = new char[3, 3];
+        public int counter = 1;
+        public int player = 1;
+        bool flag = true;
+    }
+}
---
+++ Lab4/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GameLibrary;
+namespace GameUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_FieldExistence()
+        {
+            Class1 Field = new Class1();
+            Assert.IsNotNull(Field);
+        }
+    }
+}
