3f883ff369b0d8aaf02624b4bd7a5af6b7775bd4
MiroshnichenkoNA <rainbow00863@mail.ru>
2022-05-09 20:02:51 +0700

    added method for ElementInsert

--- Lab4/Class1.cs
+++ Lab4/Class1.cs
@@ -22,5 +22,40 @@ namespace GameLibrary
                 }
             }
         }
+        public void Display()
+        {
+            Console.WriteLine("Текущее состояние поля:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(field[i, j] + "   ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public void Game_State()
+        {
+            Console.WriteLine("Ход: " + counter + " " + " Игрок номер: " + player);
+            counter++;
+            if (player == 1) player++;
+            else player--;
+        }
+
+        public void Insert_Element(char elem, int x, int y)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (field[y - 1, x - 1] == '*')
+                    {
+                        field[y - 1, x - 1] = elem;
+                    }
+                    else { };
+                }
+            }
+        }
     }
 }
